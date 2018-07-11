20d201c73c8bb726b4ff11404cf71fb8668f4ce2
Rigoberto Saenz <beto456789@gmail.com>
2018-07-11 16:26:02 -0500

    Fighter Jet added Simple vertical and horizontal motion added using forces

---
+++ AirCombat/AirCombat/Assets/Scripts/FighterSimpleMotion.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FighterSimpleMotion : MonoBehaviour {
+
+    float verticalSpeed = 18;
+    float horizontalSpeed = 15;
+    float forceValue = 25;
+    ForceMode mode = ForceMode.Acceleration;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+        if (Input.GetKey(KeyCode.UpArrow)) {
+            //this.transform.Translate(Vector3.up * Time.deltaTime * verticalSpeed);
+            this.GetComponent<Rigidbody>().AddForce(Vector3.up * forceValue, mode);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            //this.transform.Translate(Vector3.down * Time.deltaTime * verticalSpeed);
+            this.GetComponent<Rigidbody>().AddForce(Vector3.down * forceValue, mode);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            //this.transform.Translate(Vector3.left * Time.deltaTime * horizontalSpeed);
+            this.GetComponent<Rigidbody>().AddForce(Vector3.left * forceValue, mode);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            //this.transform.Translate(Vector3.right * Time.deltaTime * horizontalSpeed);
+            this.GetComponent<Rigidbody>().AddForce(Vector3.right * forceValue, mode);
+        }
+    }
+}
